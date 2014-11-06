fda50477c76617d53bbc7ba705c6e4e96fc14424
Surya Kiran <suryakiran_munukutla@intuit.com>
2014-11-05 21:12:47 -0800

    Java file

---
+++ src/main/java/com/intuit/tools/ask/Oauth/Authorization.java
@@ -0,0 +1,25 @@
+package com.intuit.tools.ask.Oauth;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+
+
+/*
+ * This class established an OAuth 2.0 authorization handshake 
+ */
+public class Authorization {
+	
+	public CloseableHttpClient getInstance() {
+		
+		
+		
+		
+		return null;
+		
+		
+	}
+	
+	
+	
+	
+
+}
